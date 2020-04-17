c8139bc8860e335419efa602a049b4b19a739225
NiuRuslan <niuruslan@gmail.com>
2020-04-17 16:45:29 +0300

    Updates

---
+++ precache-manifest.59fa29d93e89d2111a86b20d69489a0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf6a1a3cf13d6420cca9d827c0e3909d",
+    "url": "/pizza-shop/index.html"
+  },
+  {
+    "revision": "379bc29a18a43eb99c88",
+    "url": "/pizza-shop/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "505338cb89c37fe1e523",
+    "url": "/pizza-shop/static/js/2.11db2231.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pizza-shop/static/js/2.11db2231.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "379bc29a18a43eb99c88",
+    "url": "/pizza-shop/static/js/main.c6ba21a7.chunk.js"
+  },
+  {
+    "revision": "4e83725b38f6c4124309",
+    "url": "/pizza-shop/static/js/runtime-main.5f5c2b7e.js"
+  }
+]);
